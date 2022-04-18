060d7f8f18abab96c8422ac889ffd34aaac21ce2
juangom1950 <juangom73@gmail.com>
2022-04-17 23:46:08 -0400

    Client-Side Functionality

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,29 +1,38 @@
-import { useState } from 'react';
-import DarkTheme from './DarkTheme';
+import { useState } from "react";
+import DarkTheme from "./DarkTheme";
 
 function loadDarkMode() {
-  if (typeof localStorage === 'undefined') {
+  // We do this because the local storage isn't define initialy on the server.
+  // If it is undefine it means that we are running initially at the server.
+  if (typeof localStorage === "undefined") {
     return false;
   }
-  const value = localStorage.getItem('darkMode');
-  return (value === null) ? false : JSON.parse(value);
+  //JSON.parse Converts a JavaScript Object Notation (JSON) string into an object.
+  // In this case it convert this value back to a boolean value.
+  const value = localStorage.getItem("darkMode");
+  return value === null ? false : JSON.parse(value);
 }
 
 function ThemeSwitch() {
   const [darkMode, setDarkMode] = useState(loadDarkMode);
 
   const handleClick = () => {
-    localStorage.setItem('darkMode', JSON.stringify(!darkMode));
+    // We don't need to ckeck here if localStorage is available on the server, becuse
+    // This click event always happen on the broser not on the server.
+    // darkMode is a boolean but localStoreage expects a string.
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
     setDarkMode(!darkMode);
   };
 
-  console.log('[ThemeSwitch] darkMode:', darkMode);
-  const text = darkMode ? 'Light Mode' : 'Dark Mode';
+  console.log("[ThemeSwitch] darkMode:", darkMode);
+  const text = darkMode ? "Light Mode" : "Dark Mode";
   return (
     <>
+      {/* suppressHydrationWarning says It is ok to override the state set in the server by the browser  */}
       <button onClick={handleClick} suppressHydrationWarning>
         {text}
       </button>
+
       <style jsx>{`
         button {
           background: none;
